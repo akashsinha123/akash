b46bbd4332afc47d51668328263a7d80fd1cfcb8
naval <navalkishoregr@gmail.com>
2015-10-09 13:49:17 +0530

    9 oct 4th commit

---
+++ wp-content/themes/akash/tag.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="container BlogListingContainer">
+    <h3 class="BlogsListingRecentPosts">Tag Archives</h3>
+    <div id="content" class="row" role="main">
+        <?php if(have_posts()) : while(have_posts()) : the_post() ?>
+            <div class="col-md-4 BlogListingImgCols">
+                <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+                <a href="<?php the_permalink(); ?>"><img src="<?= $feat_image ?>" alt="BlogListing" class="BlogListingImg"></a>
+                <h4><a href="<?php the_permalink(); ?>" class="title"><?php the_title(); ?></a></h4>
+            </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
